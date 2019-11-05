2d81c6ed7e7bd1abd16d4d00099b55944e8a2eee
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-06 03:34:58 +0530

    Refactor ButtonPanel to class component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,38 +3,42 @@ import Button from './Button';
 
 import './ButtonPanel.css';
 
-const ButtonPanel = () => (
-  <div className='ButtonPanel'>
-    <div className='ButtonPanel-group'>
-      <Button name='AC' color='#dfd4d4' />
-      <Button name='+/-' color='#dfd4d4' />
-      <Button name='%' color='#dfd4d4' />
-      <Button name='÷' />
-    </div>
-    <div className='ButtonPanel-group'>
-      <Button name='7' color='#dfd4d4' />
-      <Button name='8' color='#dfd4d4' />
-      <Button name='9' color='#dfd4d4' />
-      <Button name='X' />
-    </div>
-    <div className='ButtonPanel-group'>
-      <Button name='4' color='#dfd4d4' />
-      <Button name='5' color='#dfd4d4' />
-      <Button name='6' color='#dfd4d4' />
-      <Button name='-' />
-    </div>
-    <div className='ButtonPanel-group'>
-      <Button name='1' color='#dfd4d4' />
-      <Button name='2' color='#dfd4d4' />
-      <Button name='3' color='#dfd4d4' />
-      <Button name='+' />
-    </div>
-    <div className='ButtonPanel-group'>
-      <Button name='0' color='#dfd4d4' wide />
-      <Button name='.' color='#dfd4d4' />
-      <Button name='=' />
-    </div>
-  </div>
-);
+class ButtonPanel extends React.Component {
+  render() {
+    return (
+      <div className='ButtonPanel'>
+        <div className='ButtonPanel-group'>
+          <Button name='AC' color='#dfd4d4' />
+          <Button name='+/-' color='#dfd4d4' />
+          <Button name='%' color='#dfd4d4' />
+          <Button name='÷' />
+        </div>
+        <div className='ButtonPanel-group'>
+          <Button name='7' color='#dfd4d4' />
+          <Button name='8' color='#dfd4d4' />
+          <Button name='9' color='#dfd4d4' />
+          <Button name='X' />
+        </div>
+        <div className='ButtonPanel-group'>
+          <Button name='4' color='#dfd4d4' />
+          <Button name='5' color='#dfd4d4' />
+          <Button name='6' color='#dfd4d4' />
+          <Button name='-' />
+        </div>
+        <div className='ButtonPanel-group'>
+          <Button name='1' color='#dfd4d4' />
+          <Button name='2' color='#dfd4d4' />
+          <Button name='3' color='#dfd4d4' />
+          <Button name='+' />
+        </div>
+        <div className='ButtonPanel-group'>
+          <Button name='0' color='#dfd4d4' wide />
+          <Button name='.' color='#dfd4d4' />
+          <Button name='=' />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ButtonPanel;
