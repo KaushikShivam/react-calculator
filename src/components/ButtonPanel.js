8ba12beeeb75985ba884b3dc9813286a49ccf1d9
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-06 03:40:23 +0530

    Add click handler to ButtonPanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,36 +4,40 @@ import Button from './Button';
 import './ButtonPanel.css';
 
 class ButtonPanel extends React.Component {
+  handleClick = buttonName => {
+    return this.props.clickHandler(buttonName);
+  };
+
   render() {
     return (
       <div className='ButtonPanel'>
         <div className='ButtonPanel-group'>
-          <Button name='AC' color='#dfd4d4' />
-          <Button name='+/-' color='#dfd4d4' />
-          <Button name='%' color='#dfd4d4' />
+          <Button name='AC' color='#dfd4d4' clickHandler={this.handleClick} />
+          <Button name='+/-' color='#dfd4d4' clickHandler={this.handleClick} />
+          <Button name='%' color='#dfd4d4' clickHandler={this.handleClick} />
           <Button name='÷' />
         </div>
         <div className='ButtonPanel-group'>
-          <Button name='7' color='#dfd4d4' />
-          <Button name='8' color='#dfd4d4' />
-          <Button name='9' color='#dfd4d4' />
+          <Button name='7' color='#dfd4d4' clickHandler={this.handleClick} />
+          <Button name='8' color='#dfd4d4' clickHandler={this.handleClick} />
+          <Button name='9' color='#dfd4d4' clickHandler={this.handleClick} />
           <Button name='X' />
         </div>
         <div className='ButtonPanel-group'>
-          <Button name='4' color='#dfd4d4' />
-          <Button name='5' color='#dfd4d4' />
-          <Button name='6' color='#dfd4d4' />
+          <Button name='4' color='#dfd4d4' clickHandler={this.handleClick} />
+          <Button name='5' color='#dfd4d4' clickHandler={this.handleClick} />
+          <Button name='6' color='#dfd4d4' clickHandler={this.handleClick} />
           <Button name='-' />
         </div>
         <div className='ButtonPanel-group'>
-          <Button name='1' color='#dfd4d4' />
-          <Button name='2' color='#dfd4d4' />
-          <Button name='3' color='#dfd4d4' />
+          <Button name='1' color='#dfd4d4' clickHandler={this.handleClick} />
+          <Button name='2' color='#dfd4d4' clickHandler={this.handleClick} />
+          <Button name='3' color='#dfd4d4' clickHandler={this.handleClick} />
           <Button name='+' />
         </div>
         <div className='ButtonPanel-group'>
-          <Button name='0' color='#dfd4d4' wide />
-          <Button name='.' color='#dfd4d4' />
+          <Button name='0' color='#dfd4d4' clickHandler={this.handleClick} wide />
+          <Button name='.' color='#dfd4d4' clickHandler={this.handleClick} />
           <Button name='=' />
         </div>
       </div>
