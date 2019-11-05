1e4f5e46b995bb9462ff1171f0feba16aaefd9e6
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-06 03:29:52 +0530

    Add base state and handle click

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,30 @@ import './App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-// import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = buttonName => {
+    this.setState({
+      state: calculate(this.state, buttonName),
+    });
+  };
+
   render() {
+    const { total, next } = this.state;
     return (
       <div className='App'>
-        <Display />
-        <ButtonPanel />
+        <Display result={next ? next : total} />
+        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
